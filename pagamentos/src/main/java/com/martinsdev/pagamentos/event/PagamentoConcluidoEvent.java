5805bbb7a49a3dd47d7c99c42f7612970044dee8
Martins20321 <jgmsilva11@gmail.com>
2026-08-17 10:59:22 -0300

    refactor(pagamentos-ms): remove Pagamento coupling from PagamentoConcluidoEvent

--- pagamentos/src/main/java/com/martinsdev/pagamentos/event/PagamentoConcluidoEvent.java
+++ pagamentos/src/main/java/com/martinsdev/pagamentos/event/PagamentoConcluidoEvent.java
@@ -3,7 +3,4 @@ package com.martinsdev.pagamentos.event;
 import com.martinsdev.pagamentos.model.Pagamento;
 
 public record PagamentoConcluidoEvent(Long pedidoId) {
-    public PagamentoConcluidoEvent(Pagamento pagamento) {
-        this(pagamento.getPedidoId());
-    }
 }
