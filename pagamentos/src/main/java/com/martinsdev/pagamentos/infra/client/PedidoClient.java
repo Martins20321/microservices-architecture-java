d052abcd4dcd4c72d05f1c5375d1ae0f8cc651c1
Martins20321 <jgmsilva11@gmail.com>
2026-08-19 09:30:39 -0300

    refactor(pagamentos-ms): remove unused recusarPagamento method from PedidoClient

--- pagamentos/src/main/java/com/martinsdev/pagamentos/infra/client/PedidoClient.java
+++ pagamentos/src/main/java/com/martinsdev/pagamentos/infra/client/PedidoClient.java
@@ -4,14 +4,10 @@ import com.martinsdev.pagamentos.infra.client.dto.PedidoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(value = "pedidos-ms", url = "${pedidos.ms.url:}")
 public interface PedidoClient {
 
     @GetMapping("/v1/pedidos/{id}")
     PedidoDTO buscarPedido(@PathVariable Long id);
-
-    @PutMapping("/v1/pedidos/{id}/recusar-pagamento")
-    void recusarPagamento(@PathVariable Long id);
 }
