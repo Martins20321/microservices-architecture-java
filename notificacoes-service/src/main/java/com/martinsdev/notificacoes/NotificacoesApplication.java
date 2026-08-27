c95c243390a6d67d386f20be888b85bf8088b1f9
Martins20321 <jgmsilva11@gmail.com>
2026-08-27 20:34:11 -0300

    feat(notificacoes-service): add Feign and webmvc dependencies with EnableFeignClients

--- notificacoes-service/src/main/java/com/martinsdev/notificacoes/NotificacoesApplication.java
+++ notificacoes-service/src/main/java/com/martinsdev/notificacoes/NotificacoesApplication.java
@@ -2,8 +2,10 @@ package com.martinsdev.notificacoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NotificacoesApplication {
 
 	public static void main(String[] args) {
