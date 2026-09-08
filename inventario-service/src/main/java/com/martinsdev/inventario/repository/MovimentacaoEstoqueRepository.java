c441ab798ac2af77009b81e07f90fc44a3bdcae4
Martins20321 <jgmsilva11@gmail.com>
2026-09-08 11:33:43 -0300

    feat(inventario-service): create MovimentacaoEstoqueRepository

---
+++ inventario-service/src/main/java/com/martinsdev/inventario/repository/MovimentacaoEstoqueRepository.java
@@ -0,0 +1,7 @@
+package com.martinsdev.inventario.repository;
+
+import com.martinsdev.inventario.model.MovimentacaoEstoque;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovimentacaoEstoqueRepository extends JpaRepository<MovimentacaoEstoque, Long> {
+}
