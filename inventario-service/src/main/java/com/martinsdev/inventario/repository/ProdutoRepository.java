127bb32ba76945ff5153b7b701ce3d24f3de8b5e
Martins20321 <jgmsilva11@gmail.com>
2026-09-08 11:33:32 -0300

    feat(inventario-service): create ProdutoRepository

---
+++ inventario-service/src/main/java/com/martinsdev/inventario/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.martinsdev.inventario.repository;
+
+import com.martinsdev.inventario.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
