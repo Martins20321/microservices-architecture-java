55f124f29059a3d1ac34d6da90ac84ed33cd2970
Martins20321 <jgmsilva11@gmail.com>
2026-09-08 18:05:34 -0300

    feat(inventario-service): add existsByNome verification method to ProdutoRepository

--- inventario-service/src/main/java/com/martinsdev/inventario/repository/ProdutoRepository.java
+++ inventario-service/src/main/java/com/martinsdev/inventario/repository/ProdutoRepository.java
@@ -4,4 +4,6 @@ import com.martinsdev.inventario.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    boolean existsByNome(String nome);
 }
