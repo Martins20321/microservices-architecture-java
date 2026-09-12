78f3539463fba5c8d0001f1399368a31e9ea967d
Martins20321 <jgmsilva11@gmail.com>
2026-09-12 11:25:19 -0300

    feat(inventario-service): add findByProdutoIdAndPedidoIdAndTipoMovimentacao to MovimentacaoEstoqueRepository

--- inventario-service/src/main/java/com/martinsdev/inventario/repository/MovimentacaoEstoqueRepository.java
+++ inventario-service/src/main/java/com/martinsdev/inventario/repository/MovimentacaoEstoqueRepository.java
@@ -1,7 +1,13 @@
 package com.martinsdev.inventario.repository;
 
 import com.martinsdev.inventario.model.MovimentacaoEstoque;
+import com.martinsdev.inventario.model.enums.TipoMovimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MovimentacaoEstoqueRepository extends JpaRepository<MovimentacaoEstoque, Long> {
+
+    // consulta jpa para buscar a combinacao de 3 campos para ter certeza na hora de confirmar reserva
+    Optional<MovimentacaoEstoque> findByProdutoIdAndPedidoIdAndTipoMovimentacao(Long produtoId, Long pedidoId, TipoMovimentacao tipoMovimentacao);
 }
