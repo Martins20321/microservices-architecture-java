ad8fa01c9f98129b6ae4245279f3f2e5e09693c8
Martins20321 <jgmsilva11@gmail.com>
2026-09-16 12:08:13 -0300

    refactor(pedidos-service): rename ItemPedidoDTO field from valor to valorUnitario

--- pedidos-service/src/main/java/com/martinsdev/pedidos/dto/ItemPedidoDTO.java
+++ pedidos-service/src/main/java/com/martinsdev/pedidos/dto/ItemPedidoDTO.java
@@ -5,7 +5,7 @@ import com.martinsdev.pedidos.model.ItemPedido;
 import java.math.BigDecimal;
 
 public record ItemPedidoDTO(Integer quantidade,
-                            BigDecimal valor) {
+                            BigDecimal valorUnitario) {
     public ItemPedidoDTO(ItemPedido itemPedido) {
         this(itemPedido.getQuantidade(), itemPedido.getValorUnitario());
     }
