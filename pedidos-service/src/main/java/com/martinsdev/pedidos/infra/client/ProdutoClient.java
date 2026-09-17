e27af1fe904ca2b82c829b8e8b557e1dee253740
Martins20321 <jgmsilva11@gmail.com>
2026-09-17 20:25:55 -0300

    feat(pedidos-service): add solicitarReservaProduto method to ProdutoClient

--- pedidos-service/src/main/java/com/martinsdev/pedidos/infra/client/ProdutoClient.java
+++ pedidos-service/src/main/java/com/martinsdev/pedidos/infra/client/ProdutoClient.java
@@ -1,12 +1,12 @@
 package com.martinsdev.pedidos.infra.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(value = "inventario-service")
 public interface ProdutoClient {
 
-    @GetMapping("/v1/produtos/{id}")
-    ProdutoDTO buscarProdutoPorId(@PathVariable Long id);
+    @PostMapping("/v1/produtos/{id}/reservar")
+    ProdutoDetailsReservaDTO solicitarReservaProduto(@PathVariable Long id, ReservarProdutoRequestDTO reservaProdutoDTO);
 }
