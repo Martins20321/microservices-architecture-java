47bf4d23fe841349a9481a65d1602fccfd802654
Martins20321 <jgmsilva11@gmail.com>
2026-09-17 20:25:32 -0300

    feat(pedidos-service): create ProdutoDetailsReservaDTO

---
+++ pedidos-service/src/main/java/com/martinsdev/pedidos/infra/client/ProdutoDetailsReservaDTO.java
@@ -0,0 +1,12 @@
+package com.martinsdev.pedidos.infra.client;
+
+import java.math.BigDecimal;
+
+public record ProdutoDetailsReservaDTO(Long idMovimentacao,
+                                       Long produtoId,
+                                       String nomeProduto,
+                                       BigDecimal valorUnitario,
+                                       Long pedidoId,
+                                       Integer quantidadeReservada,
+                                       Integer quantidadeAtualDisponivel) {
+}
